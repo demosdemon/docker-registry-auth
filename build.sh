ab03f736a5a6038b42f56918d74ff2dac54bd702
Brandon LeBlanc <brandon@platform.sh>
2019-04-07 18:44:48 -0500

    move build statements around

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ set -eux
 
 mkdir -p bin
 
-go build -o bin/config ./cmd/config
-
 GOPATH=$(mktemp -d --tmpdir go.XXXXXX)
 export GOPATH
 export PATH="$GOPATH/bin:$PATH"
 # cleanup, not really necessary on platform but nice when testing locally
 trap 'rm -rf "$GOPATH"' EXIT
+
+(cd ./cmd/config && go build -o ../../bin/config .)
 
 # unset GO111MODULE as `docker_auth` doesn't support it yet
 unset GO111MODULE
